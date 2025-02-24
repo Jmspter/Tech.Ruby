e2af00d7b51e6f18c7a6b13a903e81e127e08d61
Jmspter <jamesideias@gmail.com>
2025-02-24 00:51:10 -0300

    remoção do tailwind e adição de filtro

--- app.rb
+++ app.rb
@@ -2,28 +2,40 @@ require 'sinatra'
 require 'rss'
 require 'net/http'
 
+# URLs dos feeds RSS que você quer agregar
 FEEDS = {
   'G1' => 'https://g1.globo.com/rss/g1/tecnologia/',
   'NoticiasAoMinuto' => 'https://www.noticiasaominuto.com.br/rss/tech',
-  'FolhaDeSãoPaulo' => 'https://feeds.folha.uol.com.br/tec/rss091.xml'
+  'Camara' => 'https://www.camara.leg.br/noticias/rss/dinamico/CIENCIA-E-TECNOLOGIA'
 }.freeze
 
+# Helpers
+helpers do
+  def options_for_select(collection, selected = nil)
+    collection.map do |item|
+      "<option value='#{item}' #{'selected' if item == selected}>#{item}</option>"
+    end.join
+  end
+end
+
+# Rotas
 get '/' do
+  # Parâmetros de filtro (se existirem)
   @fonte = params['fonte']
-  @categoria = params['categoria']
 
   @noticias = []
 
   FEEDS.each do |fonte, feed_url|
+    # Aplica o filtro de fonte, se existir
     next if @fonte && fonte.downcase != @fonte.downcase
 
+    # Faz a requisição HTTP usando net/http
     uri = URI.parse(feed_url)
     response = Net::HTTP.get_response(uri)
 
     if response.is_a?(Net::HTTPSuccess)
       rss = RSS::Parser.parse(response.body, false)
       rss.items.each do |item|
-        next if @categoria && !item.categories.any? { |cat| cat.content.downcase.include?(@categoria.downcase) }
 
         @noticias << {
           fonte: fonte,
@@ -31,12 +43,12 @@ get '/' do
           link: item.link,
           descricao: item.description,
           data: item.pubDate,
-          categorias: item.categories.map(&:content)
         }
       end
     end
   end
 
+  # Ordena as notícias por data (mais recentes primeiro)
   @noticias.sort_by! { |noticia| noticia[:data] }.reverse!
 
   erb :index
